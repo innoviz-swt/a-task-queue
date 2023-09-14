b99d78641c5a5a00960c3a071fdbffef34b13c04
kolodkin <mark.kolodkin@gmail.com>
2023-09-14 10:57:06 +0300

    jobs: update pre commit hook

---
+++ schema/schema_v3.sql
@@ -0,0 +1,17 @@
+CREATE TABLE schema_version(version INTEGER PRIMARY KEY);
+CREATE TABLE jobs(jid INTEGER PRIMARY KEY, name TEXT, description TEXT);
+CREATE TABLE tasks(
+  tid INTEGER PRIMARY KEY,
+  name TEXT,
+  level REAL,
+  entrypoint TEXT NOT NULL,
+  targs MEDIUMBLOB,
+  status TEXT CHECK(status in("pending", "running", "success", "failure")),
+  take_time DATETIME,
+  start_time DATETIME,
+  done_time DATETIME,
+  pulse_time DATETIME,
+  description TEXT,
+  jid INTEGER NOT NULL,
+  FOREIGN KEY(jid) REFERENCES jobs(jid)
+);
